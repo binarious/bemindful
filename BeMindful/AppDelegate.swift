ce8c769714f66f3d3c80cc9c4d238e2083f59947
Martin B <bieder.martin@googlemail.com>
2015-01-27 08:20:24 +0100

    added calculation logic

--- BeMindful/AppDelegate.swift
+++ BeMindful/AppDelegate.swift
@@ -16,27 +16,64 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     var statusBar = NSStatusBar.systemStatusBar()
     var statusBarItem : NSStatusItem = NSStatusItem()
-    
     var active : Int = 0
+    var idleTime : Double = 0.0
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+        // initially set status bar text
         statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.title = "0m"
+
         
-        var idleTimer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("updateIdle"), userInfo: nil, repeats: true)
-        var activeTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updateActive"), userInfo: nil, repeats: true)
+        // check idle time every minute
+        var idleTimer = NSTimer.scheduledTimerWithTimeInterval(
+            1, // test with 1 sec
+            target: self,
+            selector: Selector("updateIdle"),
+            userInfo: nil,
+            repeats: true
+        )
+        
+        // count every minute
+        var activeTimer = NSTimer.scheduledTimerWithTimeInterval(
+            1.2, // test with 1.2 sec
+            target: self,
+            selector: Selector("updateActive"),
+            userInfo: nil,
+            repeats: true
+        )
         
     }
     
+    /**
+     * Determine active time and display it in the status bar
+     */
     func updateActive () {
-        active = active + 1
         
-        statusBarItem.title = "T: " + String(active)
+        if idleTime < 5.0 {
+            active = active + 1
+        }
+        
+        var activeMin  = active % 60
+        var activeHour = floor(Double(active) / 60)
+        var activeStr  = ""
+        
+        if activeHour > 0 {
+            activeStr = NSString(format: "%.0f", activeHour) + "h "
+        }
+        
+        activeStr += String(activeMin) + "m"
+        statusBarItem.title = activeStr
     }
     
+    /**
+     * If idle time > 5 minutes then reset the active time
+     */
     func updateIdle () {
-        let idleTimeMouse = CGEventSourceSecondsSinceLastEventType(0, kCGEventMouseMoved)
+        // collect mouse and keyboard events
+        let idleTimeMouse    = CGEventSourceSecondsSinceLastEventType(0, kCGEventMouseMoved)
         let idleTimeKeyboard = CGEventSourceSecondsSinceLastEventType(0, kCGEventKeyDown)
-        var idleTime = 0.0
         
         if idleTimeKeyboard > idleTimeMouse {
             idleTime = idleTimeMouse
@@ -44,13 +81,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             idleTime = idleTimeKeyboard
         }
         
-        if idleTime > 5.0 {
+        if idleTime >= 5.0 {
             active = 0
         }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
     }
 
 
